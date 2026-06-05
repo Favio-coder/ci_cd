b57fb48aaac616ad21d386faebf727d982be5d64
Favio-coder <faviusam@gmail.com>
2026-06-05 04:12:09 -0500

    nuevo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <p>Modificación!!!</p>
+        <p class="text-green-500 text-2xl">Modificación!!! Nuevo!!!</p>
     </x-slot>
 
     <div class="py-12">
